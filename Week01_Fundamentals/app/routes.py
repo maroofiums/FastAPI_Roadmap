c496458e5fd8eefa6178300ce5f0344645214a97
maroof2424 <maroof96965@gmail.com>
2026-03-14 12:28:25 +0500

    Week1 Day2

--- Week01_Fundamentals/app/routes.py
+++ Week01_Fundamentals/app/routes.py
@@ -1,1 +1,24 @@
-# routes.py
+from fastapi import APIRouter,Path,Query
+
+route = APIRouter()
+
+@route.get("/")
+def greet():
+    return {"msg":"Hello"}
+
+@route.get("/ping")
+def pong():
+    return {"msg":"pong"}
+
+@route.get("/health")
+def healthy():
+    return {"msg":"healthy"}
+
+@route.get("/items/{item_id}")
+def read_item(item_id: int = Path(..., title="The ID of the item to get", ge=0, le=1000)):
+        return {"item_id": item_id}
+
+@route.get("/search")
+def search_items(q: str = Query(..., min_length=3, max_length=50, regex="^[a-zA-Z0-9]+$")):
+    return {"q": q}
+
--- Week01_Fundamentals/main.py
+++ Week01_Fundamentals/main.py
@@ -1,15 +1,6 @@
+from app.routes import route
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def greet():
-    return {"msg":"Hello"}
-
-@app.get("/ping")
-def pong():
-    return {"msg":"pong"}
-
-@app.get("/health")
-def healthy():
-    return {"msg":"healthy"}
+app.include_router(route)
