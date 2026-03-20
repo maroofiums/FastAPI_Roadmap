2be6402503a5117eb2b79c4e6b3a82429c7743fd
maroof2424 <maroof96965@gmail.com>
2026-03-20 16:04:06 +0500

    Week2 Day2

--- Week02_CRUD_Pydantic_Routers/app/crud.py
+++ Week02_CRUD_Pydantic_Routers/app/crud.py
@@ -1,1 +1,27 @@
-# crud.py
+posts_db = [
+    {"id":1,"title":"First Post","content":"Hallo"},
+    {"id":2,"title":"Second Post","content":"Hola"}
+]
+
+def get_all_posts():
+    return posts_db
+
+def get_post_by_id(post_id:int):
+    for post in posts_db:
+        if post["id"] == post_id:
+            return post
+        
+    return None
+
+def create_post(title: str, content: str):
+    if posts_db:
+        new_id = posts_db[-1]["id"] + 1
+
+    new_post = {
+        "id": new_post,
+        "title": title,
+        "content": content
+    }
+
+    posts_db.append(new_post)
+    return new_post
--- Week02_CRUD_Pydantic_Routers/app/routers/posts.py
+++ Week02_CRUD_Pydantic_Routers/app/routers/posts.py
@@ -1,7 +1,25 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,HTTPException
+from app.crud import get_all_posts,get_post_by_id,create_post
 
 router = APIRouter(prefix="/posts",tags=["Posts"])
 
-@router.get("/")
+@router.get("/test")
 def test():
     return {"message":"Posts Routes Running..."}
+
+@router.get("/")
+def get_posts():
+    return get_all_posts()
+
+@router.get("/{post_id}")
+def get_post(post_id: int):
+    post = get_all_posts(post_id)
+
+    if not post:
+        raise HTTPException(status_code=404,detail="Post not found")
+    
+    return post
+
+@router.post("/")
+def create_posts(title: str, content:str):
+    return create_post(title=title,content=content)
