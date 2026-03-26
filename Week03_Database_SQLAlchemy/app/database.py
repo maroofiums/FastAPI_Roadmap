f936a7bb7662f5dfab0a963c20676cdf948cd1b1
M.Maroof <maroof96965@gmail.com>
2026-03-26 18:02:20 +0500

    Week3 Day1

--- Week03_Database_SQLAlchemy/app/database.py
+++ Week03_Database_SQLAlchemy/app/database.py
@@ -1,1 +1,26 @@
-# database.py
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import create_engine
+
+DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args = {'check_same_thread':False}
+)
+
+SessionLocal = sessionmaker(
+    bind = engine,
+    autoflush=False,
+    autocommit = False
+)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close
+
+
