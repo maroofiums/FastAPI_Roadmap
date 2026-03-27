991b342b0127dc92148e5cb13791b255fd9a2ace
M.Maroof <maroof96965@gmail.com>
2026-03-27 18:07:16 +0500

    Week3 Day2

--- Week03_Database_SQLAlchemy/app/models.py
+++ Week03_Database_SQLAlchemy/app/models.py
@@ -1,1 +1,20 @@
-# models.py
+from app.database import Base
+from sqlalchemy import Column,String,Integer,ForeignKey
+
+class User(Base):
+    __tabelname__ = "users"
+
+    id = Column(Integer,primary_key=True)
+    name = Column(String)
+    
+    posts = relationship("Post", back_populates="owner")
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer,primary_key=True)
+    title = Column(String)
+
+    user_id = Column(Integer,ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="posts")
