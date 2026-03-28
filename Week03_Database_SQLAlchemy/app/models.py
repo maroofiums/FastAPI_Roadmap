b2556e94de311cd4e41be2b2f6de443b40ee4aa3
maroof2424 <maroof96965@gmail.com>
2026-03-28 14:35:15 +0500

    Week3 Day3

--- Week03_Database_SQLAlchemy/app/models.py
+++ Week03_Database_SQLAlchemy/app/models.py
@@ -4,17 +4,14 @@ from sqlalchemy import Column,String,Integer,ForeignKey
 class User(Base):
     __tabelname__ = "users"
 
-    id = Column(Integer,primary_key=True)
-    name = Column(String)
-    
-    posts = relationship("Post", back_populates="owner")
+    id = Column(Integer,primary_key=True,index=True)
+    name = Column(String,nullable=False)
+    email = Column(String,nullable=False,unique=True,index=True)
 
 class Post(Base):
     __tablename__ = "posts"
 
     id = Column(Integer,primary_key=True)
-    title = Column(String)
-
+    title = Column(String,nullable=False)
+    content = Column(String,nullable=False)
     user_id = Column(Integer,ForeignKey("users.id"))
-
-    owner = relationship("User", back_populates="posts")
