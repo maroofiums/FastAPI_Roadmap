c015c47734f5ed34d0bcd204928aa7cbe260c13b
maroof2424 <maroof96965@gmail.com>
2026-04-01 16:43:54 +0500

    Week3 Day6

--- Week03_Database_SQLAlchemy/app/routers/posts.py
+++ Week03_Database_SQLAlchemy/app/routers/posts.py
@@ -1,26 +1,50 @@
+from fastapi import APIRouter, Depends, HTTPException, Query
+from sqlalchemy.orm import Session
+
 from app.crud import create_post, get_posts, get_post_by_user, delete_post
 from app.database import get_session
 from app.models import Post
-from fastapi import APIRouter, Depends, HTTPException
 
 router = APIRouter(prefix="/posts", tags=["posts"])
 
-@router.post("/", response_model=Post)
-def create_post_endpoint(post: Post, session=Depends(get_session)):
+
+@router.post("/")
+def create_post_endpoint(
+    post: Post,
+    session: Session = Depends(get_session)
+):
     return create_post(session, post)
 
-@router.get("/", response_model=list[Post])
-def get_posts_endpoint(session=Depends(get_session)):
-    return get_posts(session)
+@router.get("/")
+def get_posts_endpoint(
+    page: int = Query(1, ge=1),
+    limit: int = Query(10, ge=1, le=100),
+    user_id: int = Query(None),
+    session: Session = Depends(get_session)
+):
+    return get_posts(
+        session,
+        page=page,
+        limit=limit,
+        user_id=user_id
+    )
 
-@router.get("/user/{user_id}", response_model=list[Post])
-def get_posts_by_user_endpoint(user_id: int, session=Depends(get_session)):
+@router.get("/user/{user_id}")
+def get_posts_by_user_endpoint(
+    user_id: int,
+    session: Session = Depends(get_session)
+):
     return get_post_by_user(session, user_id)
 
+
 @router.delete("/{post_id}")
-def delete_post_endpoint(post_id: int, session=Depends(get_session)):
+def delete_post_endpoint(
+    post_id: int,
+    session: Session = Depends(get_session)
+):
     post = delete_post(session, post_id)
+
     if not post:
         raise HTTPException(status_code=404, detail="Post not found")
-    return {"detail": "Post deleted successfully"}
 
+    return {"detail": "Post deleted successfully"}
