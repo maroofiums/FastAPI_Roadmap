f6d56da5d753afb25c3489c4b98f36b170c1b5d3
Maroof <maroof96965@gmail.com>
2026-04-04 13:15:15 +0500

    Week4 Day3

--- Week04_Authentication_JWT/app/crud.py
+++ Week04_Authentication_JWT/app/crud.py
@@ -1,1 +1,15 @@
-# crud.py
+from sqlmodel import Session
+from app.models import User
+from app.core.security import get_password_hash
+
+def create_user(db: Session, username: str,email:str, password: str) -> User:
+    hashed_password = get_password_hash(password)
+    user = User(
+        username=username, 
+        email=email,
+        hashed_password=hashed_password
+        )
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
--- Week04_Authentication_JWT/app/routers/auth.py
+++ Week04_Authentication_JWT/app/routers/auth.py
@@ -1,1 +1,21 @@
-# auth.py
+from fastapi import APIRouter, Depends, HTTPException
+from sqlmodel import Session
+from app.database import get_session
+from app.crud import create_user
+from app.schemas import UserCreate,UserRead
+
+router = APIRouter()
+
+@router.post("/register", response_model=UserRead)
+def register(user: UserCreate, db: Session = Depends(get_session)):
+    
+    db_user = create_user(
+        db=db, 
+        username=user.username, 
+        email=user.email, 
+        password=user.password
+    )
+
+    return db_user
+
+
